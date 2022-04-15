18dda338eb5ec87ac65d19b62a6ce4d31ab05b49
Casey Manning <kcgrind@gmail.com>
2022-04-15 00:22:26 -0700

    initial commit

---
+++ Assets/fingor.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class fingor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<XRDirectInteractor>().interactionManager = GameObject.FindObjectOfType<XRInteractionManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
