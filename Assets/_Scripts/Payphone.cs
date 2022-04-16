01dddd4caa8cfa950c6b6ccbc24422d25d48d7a5
Casey Manning <kcgrind@gmail.com>
2022-04-15 17:41:35 -0700

    environment walls + tvs

---
+++ Assets/_Scripts/Payphone.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Payphone : MonoBehaviour
+{
+
+    public int numPressed = 0;
+
+    public AudioClip dialSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void buttonPress()
+    {
+        numPressed += 1;
+
+        if(numPressed >= 10)
+        {
+            makeCall();
+        }
+    }
+
+    public void makeCall()
+    {
+        AudioSource source = GetComponent<AudioSource>();
+        source.PlayOneShot(dialSound);
+    }
+}
